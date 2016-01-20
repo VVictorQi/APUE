93374b8ae0c28f07e2a4175076244e9985eb8ee6
Victor Qi <987038786@qq.com>
2016-01-20 20:38:17 +0800

    the envirment of apue.h and error.c

--- 10Signal/03-10_2Mysignal.c
+++ 10Signal/03-10_2Mysignal.c
@@ -1,33 +1,38 @@
-#include"apue.h"
-#include<unistd.h>
+#include<signal.h>
+#include<string.h>
+#include<stdio.h>
 #include<stdlib.h>
+#include<errno.h>
+#include<assert.h>
 static void sigusr(int signo);//信号处理函数
 int main(void)
 {
+	printf("nihaoshije");
 	if(signal(SIGINT,sigusr)==SIG_ERR)
-		err_sys("can't catch SIGUSR1\n");
+		printf("can't catch SIGUSR1\n");
 	if(signal(SIGUSR2,sigusr)==SIG_ERR)
-		err_sys("can't catch SIGUSR2\n");
-	
-	/*if((signal(SIGINT,SIG_IGN))!=SIG_IGN)
-		signal(SIGINT,sigusr);
-	if((signal(SIGUSR2,SIG_IGN))!=SIG_IGN)
-		signal(SIGUSR2,sigusr);
-*/
-//	for( ; ; )//无限循环语句，在早期程序员年代流传着这样比while(1)循环更有效率的说法，事实上也正是如此，但是在现在优化的结果两者是相同的
-	while(1)
-	{sleep(100);}
-	exit(0);
+		printf("can't catch SIGUSR2\n");	
+//	assert(signal(SIGINT,sigusr)==sigusr);
+////	if((signal(SIGINT,SIG_IGN))!=SIG_IGN)
+//		signal(SIGINT,sigusr);
+//	if((signal(SIGUSR2,SIG_IGN))!=SIG_IGN)
+//		signal(SIGUSR2,sigusr);
+
+	for( ; ; )
+		pause();
+	//无限循环语句，在早期程序员年代流传着这样比while(1)循环更有效率的说法，事实上也正是如此，但是在现在优化的结果两者是相同
+		
+
 }
 
-static void  sigusr(int signo)
+static void  sigusr(int s)
 {
-	printf("right finished");
-	if(signo == SIGINT)
+	printf("i come back");
+	if(s == SIGINT)
 		printf("SIGINT\n");
-	else if(signo == SIGUSR2)
+	else if(s == SIGUSR2)
 		printf("SIGUSR2\n");
 	else 
-		err_dump("recevied  %d \n",signo);
+		printf("recevied  %d \n",s);
 }
 
---
+++ error.c
@@ -0,0 +1,128 @@
+#include "apue.h"
+#include <errno.h>		/* for definition of errno */
+#include <stdarg.h>		/* ISO C variable aruments */
+
+static void	err_doit(int, int, const char *, va_list);
+/*
+ * Nonfatal error related to a system call.
+ * Print a message and return.
+ */
+void
+err_ret(const char *fmt, ...)
+{
+	va_list		ap;
+
+	va_start(ap, fmt);
+	err_doit(1, errno, fmt, ap);
+	va_end(ap);
+}
+
+/*
+ * Fatal error related to a system call.
+ * Print a message and terminate.
+ */
+void
+err_sys(const char *fmt, ...)
+{
+	va_list		ap;
+
+	va_start(ap, fmt);
+	err_doit(1, errno, fmt, ap);
+	va_end(ap);
+	exit(1);
+}
+
+/*
+ * Nonfatal error unrelated to a system call.
+ * Error code passed as explict parameter.
+ * Print a message and return.
+ */
+void
+err_cont(int error, const char *fmt, ...)
+{
+	va_list		ap;
+
+	va_start(ap, fmt);
+	err_doit(1, error, fmt, ap);
+	va_end(ap);
+}
+
+/*
+ * Fatal error unrelated to a system call.
+ * Error code passed as explict parameter.
+ * Print a message and terminate.
+ */
+void
+err_exit(int error, const char *fmt, ...)
+{
+	va_list		ap;
+
+	va_start(ap, fmt);
+	err_doit(1, error, fmt, ap);
+	va_end(ap);
+	exit(1);
+}
+
+/*
+ * Fatal error related to a system call.
+ * Print a message, dump core, and terminate.
+ */
+void
+err_dump(const char *fmt, ...)
+{
+	va_list		ap;
+
+	va_start(ap, fmt);
+	err_doit(1, errno, fmt, ap);
+	va_end(ap);
+	abort();		/* dump core and terminate */
+	exit(1);		/* shouldn't get here */
+}
+
+/*
+ * Nonfatal error unrelated to a system call.
+ * Print a message and return.
+ */
+void
+err_msg(const char *fmt, ...)
+{
+	va_list		ap;
+
+	va_start(ap, fmt);
+	err_doit(0, 0, fmt, ap);
+	va_end(ap);
+}
+
+/*
+ * Fatal error unrelated to a system call.
+ * Print a message and terminate.
+ */
+void
+err_quit(const char *fmt, ...)
+{
+	va_list		ap;
+
+	va_start(ap, fmt);
+	err_doit(0, 0, fmt, ap);
+	va_end(ap);
+	exit(1);
+}
+
+/*
+ * Print a message and return to caller.
+ * Caller specifies "errnoflag".
+ */
+static void
+err_doit(int errnoflag, int error, const char *fmt, va_list ap)
+{
+	char	buf[MAXLINE];
+
+	vsnprintf(buf, MAXLINE-1, fmt, ap);
+	if (errnoflag)
+		snprintf(buf+strlen(buf), MAXLINE-strlen(buf)-1, ": %s",
+		  strerror(error));
+	strcat(buf, "\n");
+	fflush(stdout);		/* in case stdout and stderr are the same */
+	fputs(buf, stderr);
+	fflush(NULL);		/* flushes all stdio output streams */
+}

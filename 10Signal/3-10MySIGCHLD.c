b5face29673ae05c8a08113e10eaf3cf219f78fd
Victor Qi <987038786@qq.com>
2016-01-21 14:29:33 +0800

    对于linux系统下SIGCLD等同于SIGCHLD，但是两者都不能正常获取信号

---
+++ 10Signal/3-10MySIGCHLD.c
@@ -0,0 +1,40 @@
+/*************************************************************************
+	> File Name: 3-10_6MySIGCHLD.c
+	> Author:Victor Qi 
+	> Mail:Victor_QiGetFuture@163.com 
+	> Created Time: Thu 21 Jan 2016 01:40:29 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+#include<apue.h>                        
+#include<sys/wait.h>                    
+static void  sig_cld(int);              
+
+int main()                              
+{                                       
+   
+    pid_t pid;                           
+    if(signal(SIGCHLD,sig_cld)==SIG_ERR) 
+        perror("signal errror");       
+    if((pid==fork())<0)                     
+        perror("fork error" );
+    else if(pid==0)
+    {
+        sleep(2);
+        _exit(0);
+    }
+    pause();
+    exit(0);
+}
+static void sig_cld(int signo)          
+{
+    pid_t pid;                          
+    int status;                         
+    printf("SIGCHLD recevied \n");       
+    if(signal(SIGCHLD,sig_cld)==SIG_ERR)
+        perror("signal error \n");
+    if((pid==wait(&status))<0)
+        perror("wait error\n");
+    printf("pid=%d\n",pid);
+ }
+

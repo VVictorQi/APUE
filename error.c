682fc9015cb6b5c55000cb717d4d2e617484516f
Victor Qi <987038786@qq.com>
2016-02-15 22:05:50 +0800

    对于error.c的内容有所扩充，将后来需要的 函数添加到error.c中并没有新建一个.c文件存放时为了在一个文件里面能够更好地辨识

--- error.c
+++ error.c
@@ -1,7 +1,7 @@
 #include "apue.h"
 #include <errno.h>		/* for definition of errno */
 #include <stdarg.h>		/* ISO C variable aruments */
-
+#include<fcntl.h>
 static void	err_doit(int, int, const char *, va_list);
 /*
  * Nonfatal error related to a system call.
@@ -125,4 +125,177 @@ err_doit(int errnoflag, int error, const char *fmt, va_list ap)
 	fflush(stdout);		/* in case stdout and stderr are the same */
 	fputs(buf, stderr);
 	fflush(NULL);		/* flushes all stdio output streams */
+}
+void pr_mask(const char *str)
+{
+    sigset_t sigset;
+    int errno_save;
+    errno_save=errno;
+    if(sigprocmask(0,NULL,&sigset))
+    {
+        err_ret("sigprocmask error");
+    }
+    else
+    {
+        printf("%s",str);
+        if(sigismember(&sigset,SIGINT))
+            printf("SIGINT");
+        if(sigismember(&sigset,SIGQUIT))
+            printf("QUIT");
+        if(sigismember(&sigset,SIGUSR1))
+            printf("USR1");
+        if(sigismember(&sigset,SIGUSR2))
+            printf("USR2");
+        printf("\n");
+    }
+    errno =errno_save;
+}
+//主要讲8.9节中说到的五个例程的实现
+//分别是TELLWAIT、TELL_PARENT、TELL_CHILD、WAIT_PARENT、WAIT_CHILD
+
+
+static volatile sig_atomic_t sigflag;
+static sigset_t newmask,oldmask,zeromask;
+
+static void sig_usr(int signo)
+{
+    sigflag=1;
+}
+void TELL_WAIT(void)
+{
+    if(signal(SIGUSR1,sig_usr)==SIG_ERR)
+        err_sys("signal sigusr1 error");
+    if(signal(SIGUSR2,sig_usr)==SIG_ERR)
+        err_sys("signal sigusr2 error");
+    sigemptyset(&newmask);
+    sigemptyset(&zeromask);
+    sigaddset(&newmask,SIGUSR1);
+    sigaddset(&newmask,SIGUSR2);
+
+    if(sigprocmask(SIG_BLOCK,&newmask,&oldmask)<0)
+        err_sys("SIG_BLOCK error");
+}
+
+void TELL_PARENT(pid_t pid)
+{
+    kill(pid,SIGUSR2);
+}
+void WAIT_PARENT(void)
+{
+    while(sigflag==0)
+        sigsuspend(&zeromask);
+    sigflag=0;
+    if(sigprocmask(SIG_SETMASK,&oldmask,NULL)<0)
+        err_sys("SIG_SETMASK error");
+}
+void TELL_CHILD(pid_t pid)
+{
+    kill(pid,SIGUSR1);
+}
+void WAIT_CHILD(void)
+{
+
+    while(sigflag==0)
+        sigsuspend(&zeromask);
+    sigflag=0;
+    if(sigprocmask(SIG_SETMASK,&oldmask,NULL)<0)
+        err_sys("SIG_SETMASK error");
+}
+void clr_fl(int fd,int flags)
+{
+    int val;
+    if((val=fcntl(fd,F_GETFL,0))<0)
+        err_sys("fcntl F_GETFL error");
+    val = ~flags;
+    if(fcntl(fd,F_SETFL,val)<0)
+        err_sys("fcntl F_SETFL error");
+}
+void set_fl(int fd,int flags)
+{
+    int val;
+    if((val=fcntl(fd,F_GETFL,0))<0)
+        err_sys("fcntl F_GETFL error");
+    val |=flags;
+    if(fcntl(fd,F_SETFL,val)<0)
+        err_sys("fcntl F_SETFL error");
+}
+int lock_reg(int fd,int cmd,int type,off_t offset ,int whence , off_t len)
+{
+    struct flock lock;
+
+
+    lock.l_type=type;
+    lock.l_start=offset;
+    lock.l_whence=whence;
+    lock.l_len=len;
+
+    //为避免每次分配flock结构的繁琐，将其动作封装成函数，一起运用，提高效率
+    //，在以后会用到
+    return fcntl(fd,cmd,&lock);
+}
+
+pid_t lock_test(int fd,int type,off_t offset ,int whence ,off_t len)
+{   
+    //函数的作用是测试一把锁，如果存在一把锁，它阻塞由参数指定的锁请求，则此函数返回持有
+    //这把锁的进程的进程ID，否则此函数返回0。
+    struct flock lock;
+    lock.l_type=type;
+    lock.l_start=offset;
+    lock.l_whence=whence;
+    lock.l_len=len;
+    if(fcntl(fd,F_GETLK,&lock)<0)//利用fcntl添加记录锁，并且记录状态
+        err_sys("fcntl error");
+    if(lock.l_type==F_UNLCK)
+        return 0;
+    return (lock.l_pid);
+}
+ssize_t readn(int fd,void *ptr ,size_t n)
+{
+    size_t      nleft;
+    ssize_t     nread;
+
+
+    nleft=n;
+    while(nleft>0)
+    {
+        if(nread=read(fd,ptr,nleft)<0)
+        {
+            if(nleft==0)
+                return -1;
+            else 
+                break;
+        }
+        else if(nread==0)
+        {
+            break;//EOF error
+        }
+        nleft-=nread;
+        ptr+=nread;
+    }
+    return (n=nleft);//return >=0
+}
+
+//改进版的read 和write函数。以为传进数据，读写指定的 数据
+ssize_t writen(int fd, const void * ptr ,size_t n)
+{
+    size_t      nleft;
+    ssize_t     nwriten;
+
+    nleft=n;
+
+    while(nleft>0)
+    {
+        if(nwriten=write(fd,ptr,nleft)<0)
+        {
+            if(nleft==n)
+                return -1;
+            else
+                break;
+        }
+        else if(nwriten==0)
+            break;
+        nleft-=nwriten;
+        ptr+=nwriten;
+    }
+    return (n-nleft);
 }

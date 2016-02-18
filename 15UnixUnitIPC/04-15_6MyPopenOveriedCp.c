5a583b1ebddca57f32c935f5370db5e1c06774ab
Victor Qi <987038786@qq.com>
2016-02-18 22:02:30 +0800

    对于本实例用popen和pclose重写，对比于 实例02-15——6明显减少了代码量，对其做了优化

---
+++ 15UnixUnitIPC/04-15_6MyPopenOveriedCp.c
@@ -0,0 +1,35 @@
+/*************************************************************************
+	> File Name: 04-15_6MyPopenOveriedCp.c
+	> Author:Victor Qi 
+	> Mail:Victor_QiGetFuture@163.com 
+	> Created Time: Thu 18 Feb 2016 09:51:19 PM CST
+ ************************************************************************/
+
+#include "apue.h" 
+#include<sys/wait.h>
+
+#define     PAGER   "${PAGER:-more}" //环境变量
+int main(int argc,char **argv)
+{
+    char line[MAXLINE];
+    FILE*fpin,*fpout;
+
+    if(argc!=2)
+        err_quit("usage:%s<pathname>",argv[0]);
+    if((fpin=fopen(argv[1],"r"))==NULL)
+        err_sys("can't open %s",argv[1]);
+    if((fpout=popen(PAGER,"w"))==NULL)
+        err_sys("powpen error");
+    
+    while(fgets(line,MAXLINE,fpin)!=NULL)
+    {
+        if(fputs(line,fpout)==EOF)
+            err_sys("fput error to pipe");
+    }
+    if(ferror(fpin))
+        err_sys("fgets error");
+    if(pclose(fpout)==-1)
+        err_sys("pclose error");
+
+    exit(0);
+}

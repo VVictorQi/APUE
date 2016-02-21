6ba27daff0d11fff12c217716c7e3067abbc5c6a
Victor Qi <987038786@qq.com>
2016-02-22 00:37:03 +0800

    虽然本实例中使用了goto，但我们也应该尽量少的使用它，虽然有时候使用goto简化了某些操作。但同时带来了不确定

---
+++ 16UnixUnitInterIPCsocket/04-16_12MyInitSocketFunction.c
@@ -0,0 +1,37 @@
+/*************************************************************************
+	> File Name: 04-16_12MyInitSocketFunction.c
+	> Author:Victor Qi 
+	> Mail:Victor_QiGetFuture@163.com 
+	> Created Time: Mon 22 Feb 2016 12:25:47 AM CST
+ ************************************************************************/
+
+#include "apue.h" 
+#include<errno.h>
+#include<sys/socket.h>
+
+int initsever(int type,const struct sockaddr *addr,socklen_t  alen)
+{
+    int fd;
+    int err=0;
+    if((fd=socket(addr->sa_family,type,0))<0)
+        return (-1);
+    if(bind(fd,addr,alen)<0)
+        goto errout;
+    if(type==SOCK_STREAM||type==SOCK_SEQPACKET)
+    {
+        if(listen (fd,alen)<0)
+            goto errout;
+    }
+    return (fd);
+errout:
+    err=errno;
+    close(fd);
+    errno=err;
+    return(-1);
+}
+//虽然C语言警告我们不要无缘无故的使用goto因为，不安全，但是如果你把c吃透，偶尔的使用也是不错的。
+//在linux系统内核中，也使用了大量的goto语句。
+//但是对于现代语言系统中goto被摒弃是确定的，而且在一些熟知的socket模型中goto也在进来那个避免少的使用
+//
+//
+//TCP由于一些奇怪的地址复用规则，使的这个例子不完备。

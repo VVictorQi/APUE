a65e5f2c762fde6ec0dbd6be8cc16a922f7be329
Victor Qi <987038786@qq.com>
2016-02-22 01:56:14 +0800

    新增了第五函数，打开标志位

---
+++ 13UnixUnitDaemonFork/05-13_9Mysetckoexec.c
@@ -0,0 +1,17 @@
+/*************************************************************************
+	> File Name: 05-13_9Mysetckoexec.c
+	> Author:Victor Qi 
+	> Mail:Victor_QiGetFuture@163.com 
+	> Created Time: Mon 22 Feb 2016 01:53:37 AM CST
+ ************************************************************************/
+
+#include "apue.h" 
+#include<fcntl.h>
+int set_cloexec(int fd)
+{
+    int val;
+    if((val=fcntl(fd,F_GETFD,0))<0)
+        return -1;
+    val |=FD_CLOEXEC;
+    return(fcntl(fd,F_SETED,val));
+}

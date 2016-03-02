6b25269c546d108178a35b9a87a54a04d2b5fd97
Victor Qi <987038786@qq.com>
2016-03-02 21:11:25 +0800

    测试指针

---
+++ UnixUnit01/Test*++.c
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: Test*++.c
+	> Author:Victor Qi 
+	> Mail:Victor_QiGetFuture@163.com 
+	> Created Time: Wed 02 Mar 2016 01:00:43 AM CST
+ ************************************************************************/ 
+#include<stdio.h>
+int main(void)
+{
+    char *ptr="Linux";
+    printf("%c",*ptr++);
+    printf("%c",*ptr);
+    return 0;
+}
